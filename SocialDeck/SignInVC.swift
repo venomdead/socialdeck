370f8826482d974e518d84ca7743de5b790bf3a1
Jorge Osuna Benitez <Jorge@Jorges-MacBook-Air.local>
2017-03-29 15:48:34 -0700

    first commit, firebase integration

---
+++ SocialDeck/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  SocialDeck
+//
+//  Created by Jorge Osuna Benitez on 3/29/17.
+//  Copyright © 2017 Jorge Osuna Benitez. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
