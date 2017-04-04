20ad7d6f43ff7ded56e11a3ddf6ca8c8b6c9abe1
Jorge Osuna Benitez <Jorge@Jorges-MacBook-Air.local>
2017-04-04 16:20:45 -0700

    sign in ui custom clases

---
+++ SocialDeck/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialDeck
+//
+//  Created by Jorge Osuna Benitez on 4/4/17.
+//  Copyright © 2017 Jorge Osuna Benitez. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0/255.0
---
+++ SocialDeck/RoundButton.swift
@@ -0,0 +1,29 @@
+//
+//  RoundButton.swift
+//  SocialDeck
+//
+//  Created by Jorge Osuna Benitez on 4/4/17.
+//  Copyright © 2017 Jorge Osuna Benitez. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        imageView?.contentMode = .scaleAspectFit
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
+    }
+}
