97be109cc6aecf3839fbbb2e04ba48dcaaafb072
Jorge Osuna Benitez <Jorge@Jorges-MacBook-Air.local>
2017-04-07 10:48:51 -0700

    completed email authentication

--- SocialDeck/SignInVC.swift
+++ SocialDeck/SignInVC.swift
@@ -13,6 +13,10 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var passwordField: FancyField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -25,7 +29,7 @@ class SignInVC: UIViewController {
     
     
 // facebook authentication
-    @IBAction func facebookBtnTapped(_ sender: AnyObject) {
+    @IBAction func facebookBtnTapped(_ sender: RoundButton) {
         
         let facebookLogin = FBSDKLoginManager()
         
@@ -58,5 +62,27 @@ class SignInVC: UIViewController {
         })
     }
 
+   
+    @IBAction func signInTapped(_ sender: AnyObject) {
+        
+        if let email = emailField.text, let pwd = passwordField.text {
+            
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("JORGE: email user authenticated with firebase")
+                }
+                else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("JORGE: unable to auth with firebase")
+                        }
+                        else{
+                            print("JORGE: Succesfully authenticated with firebase")
+                        }
+                    })
+                }
+            })
+        }
+    }
 }
 
