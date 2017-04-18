dd5ad1c30549f324b3a159829ce5a0453d15ee26
Jorge Osuna Benitez <Jorge@Jorges-MacBook-Air.local>
2017-04-18 16:11:50 -0700

    Added some ui design to feed screen

--- SocialDeck/CircleView.swift
+++ SocialDeck/CircleView.swift
@@ -1,26 +1,32 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  SocialDeck
 //
-//  Created by Jorge Osuna Benitez on 4/4/17.
+//  Created by Jorge Osuna Benitez on 4/18/17.
 //  Copyright © 2017 Jorge Osuna Benitez. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
         
     }
     
 
+    
+    
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+
+    }
 }
