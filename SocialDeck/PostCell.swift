22d7beb1347a9e96c76d01750ae65bfe867069e6
Jorge Osuna Benitez <Jorge@Jorges-MacBook-Air.local>
2017-04-19 16:32:01 -0700

    Added table view to news feed

---
+++ SocialDeck/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  SocialDeck
+//
+//  Created by Jorge Osuna Benitez on 4/18/17.
+//  Copyright © 2017 Jorge Osuna Benitez. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+   
+   
+
+}
